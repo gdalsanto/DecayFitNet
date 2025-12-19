ec86cfeb922ca81b50d2c2b898b675aa5d4bac34
Gloria Dal Santo <gloria.dalsanto@outlook.it>
2025-12-19 08:34:39 +0200

    Remove /matlab and /data

--- matlab/decayTime2DecayRate.m
+++
@@ -1,5 +0,0 @@
-function decayRate = decayTime2DecayRate(decayTime, fs)
-% Convert between exp((-13.8*t)/(fs*decayTime)) and exp(-decayRate*t) with
-% t being time in samples
-decayRate = log(1e6)./decayTime/fs;
-end
--- matlab/schroederT2EnvelopeT.m
+++
@@ -1,7 +0,0 @@
-function envelopeT = schroederT2EnvelopeT(schroederT)
-assert(any(size(schroederT)==1), 'schroederT must be a row vector.');
-
-% Envelope is in linear scale, not quadratic, therefore decay rates halve, 
-% and T values double
-envelopeT = 2*schroederT;
-end
